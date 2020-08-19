fd4abac9011b92a99943c2b54a9c824e4cc272a4
Gareth Wright <garpunkal@gmail.com>
2020-08-19 17:07:51 +0100

    bootstrap

--- src/frontend/gulpfile.js/tasks/stylesheets.js
+++ src/frontend/gulpfile.js/tasks/stylesheets.js
@@ -1,11 +1,10 @@
 const gulp = require("gulp");
 const gulpif = require("gulp-if");
 const sass = require("gulp-sass");
-const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
 const sourcemaps = require("gulp-sourcemaps");
 const postcss = require("gulp-postcss");
 const postCssImport = require('postcss-import');
-const tailwindcss = require('tailwindcss');
 const postcssPresetEnv = require('postcss-preset-env');
 const globalPaths = require("../../package.json").paths;
 const flags = require("../config/flags");
@@ -14,24 +13,22 @@ gulp.task("stylesheets", function () {
 
   return gulp
     .src(globalPaths.css.source + globalPaths.css.filter)
-    
-    .pipe(gulpif(flags.maps, sourcemaps.init()))
-   
-    .pipe(plumber())
-   
-    .pipe(sass({
-       includePaths: ["node_modules", "scss"]
-      }).on("error", sass.logError))
 
-    .pipe(postcss([postCssImport(), tailwindcss(), postcssPresetEnv({
+    .pipe(gulpif(flags.maps, sourcemaps.init()))
+
+    .pipe(sass({
+      includePaths: ["node_modules", "scss"]
+    }).on("error", sass.logError))
+
+    .pipe(postcss([autoprefixer(), postCssImport(), postcssPresetEnv({
       stage: 1 //https://preset-env.cssdb.org/features#stage-1
     })]))
-  
+
     .pipe(gulpif(flags.minify, sass({
       outputStyle: "compressed"
     })))
-    
+
     .pipe(gulpif(flags.maps, sourcemaps.write("maps")))
-  
+
     .pipe(gulp.dest(globalPaths.build + globalPaths.css.destination));
 });
