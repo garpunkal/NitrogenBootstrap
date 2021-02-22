828432b2e9a6b77ba0a1e7a7c5b9127176163565
Gareth Wright <garpunkal@gmail.com>
2021-02-22 10:29:28 +0000

    fix

--- src/frontend/gulpfile.js/tasks/stylesheets.js
+++ src/frontend/gulpfile.js/tasks/stylesheets.js
@@ -6,34 +6,31 @@ const sourcemaps = require("gulp-sourcemaps");
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const purgecss = require("gulp-purgecss");
-const postcssPresetEnv = require("postcss-preset-env");
+const postcssPresetEnv = require('postcss-preset-env');
 const globalPaths = require("../../package.json").paths;
 const flags = require("../config/flags");
 const purgeWhiteList = require("../../src/scss/whitelist.json");
 
 gulp.task("stylesheets", function () {
+
   const postCssPlugins = [
     autoprefixer(),
     postcssPresetEnv({
-      stage: 1, //https://preset-env.cssdb.org/features#stage-1
-    }),
+      stage: 1 //https://preset-env.cssdb.org/features#stage-1
+    })
   ];
 
   if (flags.minified)
-    postCssPlugins.push(
-      cssnano({ preset: ["default", { discardComments: { removeAll: true } }] })
-    );
+    postCssPlugins.push(cssnano({preset: ["default", { discardComments: { removeAll: true }}]}));
 
   return gulp
     .src(globalPaths.css.source + globalPaths.css.filter)
-    .pipe(gulpif(flags.maps, sourcemaps.init()))   
-    .pipe(
-      sass({ includePaths: ["node_modules", "scss"] }).on(
-        "error",
-        sass.logError
-      )
-    )
+
+    .pipe(gulpif(flags.maps, sourcemaps.init()))
+
+    .pipe(sass({ includePaths: ["node_modules", "scss"]}).on("error", sass.logError))
     .pipe(postcss(postCssPlugins))
+
     .pipe(
       gulpif(
         flags.purge,
