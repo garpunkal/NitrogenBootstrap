226b665f7f7728a57cead263d06f6a84dfe13a84
Gareth Wright <garpunkal@gmail.com>
2021-02-26 14:13:18 +0000

    removed redundancy

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, '_dist')
   },
   mode: flags.minified ? "production" : "development"  
 };
