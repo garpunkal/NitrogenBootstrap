0cf13258bbc40da5741a13d4f074b48c8e5c3976
Gareth Wright <garpunkal@gmail.com>
2021-02-26 14:36:40 +0000

    Revert "removed redundancy"
    This reverts commit 226b665f7f7728a57cead263d06f6a84dfe13a84.

--- src/frontend/webpack.config.js
+++ src/frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, '_dist')
+    path: path.resolve(__dirname, 'dist')
   },
   mode: flags.minified ? "production" : "development"  
 };
